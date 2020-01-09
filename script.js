846dc2b0b40816327e6090dfd4e67143bb8a04b2
Adam DeLessio <adamdelessio@gmail.com>
2020-01-09 13:26:36 -0500

    will alert when it's a tie

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let redTurn = true
+let displayRed = document.querySelector('.red-turn')
+let displayBlue = document.querySelector('.blue-turn')
 
 let grid = document.querySelector('.grid-container')
 function startGame() {
@@ -6,41 +8,75 @@ function startGame() {
 		let square = document.createElement('div')
 		square.setAttribute('class', 'square')
 		square.setAttribute('id', 'grid' + i)
-		square.setAttribute('data-id', 0)
+		square.setAttribute('data-id', i)
 		square.style.backgroundColor = 'white'
 		square.addEventListener('click', changeColor)
 		grid.appendChild(square)
 	}
+	displayRed.style.display = 'block'
 }
+
+let redArray = []
+let blueArray = []
 
 function changeColor(event) {
 	if (redTurn === true && event.target.style.backgroundColor === 'white') {
 		event.target.style.backgroundColor = 'red'
+		displayBlue.style.display = 'block'
+		displayRed.style.display = 'none'
+		redArray.push(event.target.getAttribute('data-id'))
+		checkWin()
+		console.log(redArray)
 		redTurn = false
-	} else if (redTurn === true && event.target.style.backgroundColor === 'blue') {
-		changeColor()
 	} else if (redTurn === false && event.target.style.backgroundColor === 'white') {
 		event.target.style.backgroundColor = 'blue'
+		displayRed.style.display = 'block'
+		displayBlue.style.display = 'none'
+		blueArray.push(event.target.getAttribute('data-id'))
+		checkWin()
+		console.log(blueArray)
 		redTurn = true
-	} else {
-		changeColor()
 	}
 }
 
+const winOptions = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
+
+function checkWin() {
+	if (redArray.length + blueArray.length === 9) {
+		alert("It's a tie")
+	}
+}
+
+
+
+
+
+
 /// Reset button
 document.querySelector('.reset').addEventListener('click', newGame)
-
 function newGame() {
 	let removeSquare = document.querySelectorAll('.square')
-	for (let i = 0; i < removeSquare.length; i++) {
-		removeSquare[i].remove()
-	}
+	removeSquare.forEach(i => {
+		i.remove()
+	})
+	displayRed.style.display = 'block'
+	displayBlue.style.display = 'none'
+	redArray = []
+	blueArray = []
+	redTurn = true
 	startGame()
 }
 
 startGame()
-
-
 
 
 
